be5d62b6d23541dfe0ebedf37868d97999a3880b
Pat Buxton <patrick.buxton@tartansolutions.com>
2021-04-08 15:13:36 +0100

    Send "render_postcompile" to compile method to achieve 1.3.* behaviour

--- plaidcloud/utilities/analyze_table.py
+++ plaidcloud/utilities/analyze_table.py
@@ -140,7 +140,7 @@ def compiled(sa_query, dialect='greenplum'):
     """
     dialect = dialect or 'greenplum'  # Just in case someone sends a blank string, or a None by mistake
     eng = sqlalchemy.create_engine(f'{dialect}://127.0.0.1/')
-    compiled_query = sa_query.compile(dialect=eng.dialect)
+    compiled_query = sa_query.compile(dialect=eng.dialect, compile_kwargs={"render_postcompile": True})
     return str(compiled_query).replace('\n', ''), compiled_query.params
 
 
