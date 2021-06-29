3e88ec4850cef7639c61009e8921458d011eff73
Adams Tower <adams.tower@tartansolutions.com>
2021-06-29 18:16:27 -0400

    Use plaid-rpc ch7827

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         'numpy;python_version>="3.5"',
         'numpy<=1.16.5;python_version<"3.5"',
         'pandas',
-        'plaidcloud-rpc@git+git://github.com/PlaidCloud/plaid-rpc.git@master#egg=plaidcloud-rpc',
+        'plaidcloud-rpc@git+git://github.com/PlaidCloud/plaid-rpc.git@ch7827#egg=plaidcloud-rpc',
         'requests',
         'orjson',
         'openpyxl',
