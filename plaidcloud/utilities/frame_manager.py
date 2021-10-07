d7274dcfed6f01d05989c11b05aed112d2f2d86d
inviscid <paul.morel@tartansolutions.com>
2021-10-07 08:19:43 -0500

    more header logging

--- plaidcloud/utilities/frame_manager.py
+++ plaidcloud/utilities/frame_manager.py
@@ -2383,7 +2383,7 @@ def excel_to_csv_xlrd(excel_file_name, csv_file_name, sheet_name='sheet1', clean
     datemode = getattr(wb, 'datemode', getattr(wb, 'date_mode', 0))
     sh = wb.sheet_by_name(sheet_name)
     column_count = sh.ncols
-    null_value = '<null>'
+    null_value = None
 
     with open(csv_file_name, 'wb') as csv_file:
         wr = csv.writer(
@@ -2415,26 +2415,28 @@ def excel_to_csv_xlrd(excel_file_name, csv_file_name, sheet_name='sheet1', clean
 
                 for col in range(0, column_count):
                     # See if this column name is a date type
-                    column_name_dtype = dtype_from_excel(sh.cell(skip_rows, col).ctype)
+                    column_name_dtype = dtype_from_excel(sh.cell(rownum, col).ctype)
                     logger.info(f'----- Column Header dtype for column {col} is {column_name_dtype}')
                     if has_header:
                         logger.info(f'----- Column Header has header flag is {has_header}')
+                        logger.info(f'----- Column Header Cell Value: {sh.cell(rownum, col).value}')
                         if column_name_dtype == 'timestamp':
                             # Need to convert this from an Excel date stamp to human readable form
-                            column_name = xlrd.xldate_as_datetime(sh.cell(skip_rows, col).value, 0).date().isoformat()
+                            column_name = xlrd.xldate_as_datetime(sh.cell(rownum, col).value, 0).date().isoformat()
                         elif column_name_dtype == 'boolean':
-                            if sh.cell(skip_rows, col).value:
+                            if sh.cell(rownum, col).value:
                                 column_name = 'TRUE'
                             else:
                                 column_name = 'FALSE'
                         else:
-                            column_name = str(sh.cell(skip_rows, col).value).strip().replace('\n', '').replace('\r', '')
+                            column_name = str(sh.cell(rownum, col).value).strip().replace('\n', '').replace('\r', '')
+                        logger.info(f'----- Raw Column name prior to compliance check {column_name}')
 
                         if column_name[0:1] in string.digits:
                             column_name = f'_{column_name}'
                         column_name = ''.join([c for c in column_name if c not in invalid_characters]) # Remove invalid characters
                         column_name = column_name[:63] # Truncate to max length
-                        data_dtype = dtype_from_excel(sh.cell(skip_rows + 1, col).ctype) # go to next row to determine dytype of the data
+                        data_dtype = dtype_from_excel(sh.cell(rownum + 1, col).ctype) # go to next row to determine dytype of the data
                         logger.info(f'----- Column name after compliance check {column_name}')
                     else:
                         column_name = None
@@ -2483,7 +2485,7 @@ def excel_to_csv_xlrd(excel_file_name, csv_file_name, sheet_name='sheet1', clean
                         dtype = dtype_from_excel(c.ctype)
                         # logger.info(f'Column: {col_pos}')
                         if dtype != col_info['dtype']:
-                            col_info['dtype'] = 'text' # elegantly handle situation where the guess was wrong initially.  must be mixed data.  default to text.
+                            column_information[col_pos]['dtype'] = 'text' # elegantly handle situation where the guess was wrong initially.  must be mixed data.  default to text.
 
                         if dtype == 'text':
                             row.append(c.value)
