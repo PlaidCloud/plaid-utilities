84b60f1bd65af7cc7800e349226e73460a0fb071
Pat Buxton <patrick.buxton@tartansolutions.com>
2021-11-10 14:34:04 +0000

    Fix having to double-specify requirements

--- setup.py
+++ setup.py
@@ -14,55 +14,37 @@ extras = {
     'test': test_deps
 }
 
-try:
-    import pygit2
-    import os
-    import datetime
-    repo = pygit2.Repository(os.getcwd())
-    commit_hash = repo.head.target
-    commit = repo[commit_hash]
-    print('commit {}'.format(commit_hash))
-    print(datetime.date.fromtimestamp(commit.commit_time))
-    print(commit.message)
-except ImportError:
-    print('pygit2 is not available. Cannot detect current commit.')
-except:
-    print('This is probably not a repo, a copy of the code.')
+# Commenting out, seems unnecessary these days
+# try:
+#     import pygit2
+#     import os
+#     import datetime
+#     repo = pygit2.Repository(os.getcwd())
+#     commit_hash = repo.head.target
+#     commit = repo[commit_hash]
+#     print('commit {}'.format(commit_hash))
+#     print(datetime.date.fromtimestamp(commit.commit_time))
+#     print(commit.message)
+# except ImportError:
+#     print('pygit2 is not available. Cannot detect current commit.')
+# except:
+#     print('This is probably not a repo, a copy of the code.')
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='plaidcloud-utilities',
-    version="1.0.1",
+    version="1.0.5",
     author='Michael Rea',
     author_email='michael.rea@tartansolutions.com',
     packages=['plaidcloud.utilities', 'plaidcloud.utilities.remote'],
-    install_requires=[
-        'argparse',
-        'chardet',
-        'ipython',
-        'minimock',
-        'numpy;python_version>="3.5"',
-        'numpy<=1.16.5;python_version<"3.5"',
-        'pandas',
-        'plaidcloud-rpc@git+git://github.com/PlaidCloud/plaid-rpc.git@v1.0.0#egg=plaidcloud-rpc',
-        'requests',
-        'orjson',
-        'openpyxl',
-        'six',
-        'sqlalchemy',
-        'sqlalchemy-hana',
-        'sqlalchemy-greenplum',
-        'texttable',
-        'toolz',
-        'unicodecsv',
-        'xlrd3@git+git://github.com/PlaidCloud/xlrd3.git@master#egg=xlrd3',
-        'xlwings;platform_system=="Windows"',
-        'pyyaml',
-    ],
+    install_requires=required,
     tests_require=test_deps,
     setup_requires=['pytest-runner'],
     extras_require=extras,
